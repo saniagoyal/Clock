bee8097b2cade69e8d63feda9199bf507507fd00
saniagoyal <124520922+saniagoyal@users.noreply.github.com>
2023-03-13 14:39:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ setInterval(() => {
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
 }, 1000);
+setInterval(()=>{
+    var dateTime=new Date();
+    var hrs= dateTime.getHours();
+    var min= dateTime.getMinutes();
+    var sec= dateTime.getSeconds();
+    var session=document.getElementById('session');
+    if(hrs>=12){
+        session.innerHTML='PM';
+    }else{
+        session.innerHTML='AM';
+    }
+    if(hrs>12){
+        hrs=hrs-12;                                                                                                                        
+    }
+    document.getElementById('hours').innerHTML=hrs;
+    document.getElementById('minutes').innerHTML=min;
+    document.getElementById('seconds').innerHTML=sec;
+},1000);
